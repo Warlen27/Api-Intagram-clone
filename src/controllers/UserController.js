f0a20732f8ac639df5bdac1bc18b62dab3583ce6
Warlen27 <warlenalves40@gamil.com>
2020-02-21 22:51:12 -0300

    controller de usuários

---
+++ src/controllers/UserController.js
@@ -0,0 +1,70 @@
+const User = require('../models/User');
+
+module.exports = {
+    async index(req, res) {
+        const users = await User.find().sort('-createdAt');
+      
+        res.json(users);
+    },
+
+    async show(req, res) {
+        const profile = await User.findById(req.params.id);
+      
+        res.json(profile);
+    },
+
+    async store(req, res) {
+            const { name, username, email, password } = req.body;
+
+          
+            const userExists = await User.findOne( { email: req.body.email } );
+
+    if (userExists) {
+      return res.status(400).json({ error: 'User already exists.' });
+    }
+   
+
+            const user = await User.create({
+                name,
+                username,
+                email,
+                password,
+                
+            });
+          
+    
+
+            return res.json(user);
+    },
+
+    async destroy(req, res) {
+        const user =  await User.findById(req.params.id);
+         
+     user.remove();
+
+
+     
+
+        return res.json(user); 
+
+    },
+
+    async update(req, res) {
+
+        const user =  await User.findById(req.params.id);
+        
+    
+       
+        for (let item in req.body ) {
+            user[item] = req.body[item];
+           
+        }
+       
+        if (req.file) {
+            const avatar = req.file.filename;
+            user.avatar = avatar;
+        }
+        await user.save()
+        return res.json(user);
+    },
+}
